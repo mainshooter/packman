db952419d1dd645e451d92f6cd5cc301d8c7dc8f
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-12 09:56:07 +0200

    Added function to create the map

---
+++ v2.js
@@ -0,0 +1,230 @@
+var map;
+// 2 dimentional array
+
+var game;
+// game object
+
+var pacman;
+// Pacman object
+
+(function() {
+  game = {
+      createMap: function() {
+        // Create the map when it loads for the first time
+
+        // This is a anominouse function
+        // You can call the function by the following
+        // game.Createmap();
+        map = new Array(10);
+        for (var i = 0; i < 10; i++) {
+          map[i] = new Array(10);
+        }
+        var div;
+        // Creates the HTML elements
+        for (var i = 0; i < map.length; i++) {
+          for (var t = 0; t < map.length; t++) {
+            div = document.createElement("div");
+
+            div.innerHTML = i + "," + t;
+            div.id = i + "," + t;
+
+            if (map[i][t] == 0) {
+              div.className = "border";
+            }
+            else if (map[i][t] == "p") {
+              div.className = "pacman"
+            }
+            else if (map[i][t] == 2) {
+              div.className = "point";
+            }
+            else if (map[i][t] == 3) {
+              div.className = "ghostPil";
+            }
+            else if (map[i][t] == 4) {
+              div.className = "ghost";
+            }
+            else {
+              div.className = "walk";
+            }
+
+            if (t === 9) {
+
+            }
+            else if (t != 9) {
+              div.className += " bord_part";
+            }
+            div.className += " bord";
+            document.getElementById("map").appendChild(div);
+          }
+        }
+      },
+      createGhost: function() {
+        // Loads the map
+        map[3][8] = 4;
+      },
+      renderMap: function() {
+        for (var i = 0; i < map.length; i++) {
+          for (var t = 0; t < map.length; t++) {
+            var div = document.getElementById(i + "," + t);
+
+            if (map[i][t] == 0) {
+              div.className = "border";
+            }
+            else if (map[i][t] == "p") {
+              div.className = "pacman"
+            }
+            else if (map[i][t] == 2) {
+              div.className = "point";
+            }
+            else if (map[i][t] == 3) {
+              // Is a pill wich if it is been eaten pacman can eat a ghost
+              div.className = "ghostPil";
+            }
+            else if (map[i][t] == 4) {
+              // Ghosts wich before the ghost stand on the block didn't contain a point
+              div.className = "ghost";
+            }
+            else if (map[i][t] == 5) {
+              // Ghosts wich before the ghost stand on the block is containing a point
+              div.className = "ghost";
+              // GHOST + point
+            }
+            else {
+              div.className = "walk";
+            }
+
+            if (t === 9) {
+              // When it is nothing
+            }
+            else if (t != 9) {
+              // When the vak isn't at the end of the map array
+              // We don't use a float
+              div.className += " bord_part";
+            }
+            div.className += " bord";
+          }
+        }
+      },
+      createBorder: function() {
+        console.log("RUN");
+        for (var i = 0; i < 9; i++) {
+          map[0][i] = 0;
+          // Creates the top border
+        }
+        for (var i = 0; i < 9; i++) {
+          map[9][i] = 0;
+          // Create the bottom border
+        }
+        for (var i = 0; i < 9; i++) {
+          map[i][9] = 0;
+          // Create right border
+        }
+        for (var i = 0; i < 9; i++) {
+          map[i][0] = 0;
+          // Create left
+        }
+        // Wall is 0
+        // Walkable path is 1
+        // Points ball is 2
+        // pacman is p
+
+        map[9][9] = 0;
+      },
+      createPoints: function() {
+        // Create the points that pacman can eat
+        for (var i = 0; i < map.length; i++) {
+          for (var t = 0; t < map.length; t++) {
+            if (map[i][t] != 0 && map[i][t] != 'p' && map[i][t] != 3 && map[i][t] != 4 && map[i][t] != 5) {
+              // Only on the thing that aren't pacman and aren't ghosts ore ghost points
+              map[i][t] = 2;
+              // 2 is a point
+            }
+          }
+        }
+      },
+      clearMap: function() {
+        for (var i = 0; i < map.length; i++) {
+          for (var t = 0; t < map.length; t++) {
+            map[i][t] = 1;
+          }
+        }
+      },
+      createGhostCandy: function() {
+        // This function creates the candy that pacman can eat a ghost
+        map[1][4] = 3;
+        map[6][6] = 3;
+        map[8][5] = 3;
+      },
+      resetMap: function() {
+        // This function resets the map when pacman dies
+        game.clearMap();
+
+        game.createMap();
+        game.createBorder();
+        game.createPoints();
+        game.createGhostCandy();
+
+        game.renderMap();
+      },
+      startMap: function() {
+        // This function starts the game
+        game.createMap();
+        game.createBorder();
+        game.createPoints();
+        game.createGhostCandy();
+
+        game.renderMap();
+      },
+      displayPoints: function() {
+        document.getElementById('id').innerHTML = "Score: " + score;
+      },
+      addScore: function() {
+        // Adds a score when pacman eats a point
+        score++;
+      },
+      detectKey: function(keycode) {
+        // Detects a key press
+        // Returns the translated keypress
+        var keytranslate;
+        switch (keycode) {
+          case 119:
+            keytranslate = 'up';
+            break;
+          case 115:
+            keytranslate = 'down';
+            break;
+          case 97:
+            keytranslate = 'left';
+            break;
+          case 100:
+            keytranslate = 'right';
+            break;
+        }
+        return(keytranslate);
+      }
+  }
+})();
+game.startMap();
+
+(function() {
+  pacman = {
+    createPacman: function() {
+      map[5][4] = "p";
+    },
+    locatePacman: function() {
+      // Locates pacman by the map array
+      for (var i = 0; i < map.length; i++) {
+        for (var t = 0; t < map.length; t++) {
+          if (map[i][t] == 'p') {
+
+            return([i, t]);
+            // y , x cordinates
+          }
+        }
+      }
+    },
+    movePacman: function() {
+
+    }
+  }
+});
